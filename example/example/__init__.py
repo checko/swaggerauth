d9239637bf031240b2b0ed3a3ac1f0d931510548
charles.chang <charles.chang@royaltek.com>
2022-01-25 13:56:08 +0800

    add CORS

--- example/example/__init__.py
+++ example/example/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import
 
 from flask import Flask
-
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 
 import v1
@@ -10,6 +10,7 @@ import v1
 
 def create_app():
     app = Flask(__name__, static_folder='static')
+    CORS(app)
     app.register_blueprint(
         v1.bp,
         url_prefix='/v1')
